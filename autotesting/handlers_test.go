3f82a36c70e7fb968725f17bd30c259ddd2ea274
Dave Stearns <dlsinfo@uw.edu>
2018-04-07 12:18:31 -0700

    HTTP handler function for automated testing demo

---
+++ autotesting/handlers_test.go
@@ -0,0 +1,13 @@
+package autotesting
+
+import (
+	"testing"
+)
+
+func TestIdenticonHandler(t *testing.T) {
+	//TODO: use the methods and structs in the
+	//net/http/httptest package to build the
+	//http.ResponseWriter and *http.Request that
+	//you need to invoke the handler function,
+	//and examine what was written to the response.
+}
