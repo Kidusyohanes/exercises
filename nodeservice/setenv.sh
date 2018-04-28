74b1efa2c22ce036e6e2ced7f3ee87273ba6c60b
Dave Stearns <dlsinfo@uw.edu>
2018-04-28 11:58:59 -0700

    no need to include port number in mysql addr

--- nodeservice/setenv.sh
+++ nodeservice/setenv.sh
@@ -1,5 +1,5 @@
 #!/bin/echo please source using `source setenv.sh`
 export MYSQL_ROOT_PASSWORD=$(openssl rand -base64 32)
 export MYSQL_DATABASE=blueit
-export MYSQL_ADDR=127.0.0.1:3306
+export MYSQL_ADDR=127.0.0.1
 export ADDR=localhost:4000
