3a5e015a5a8bed6bb2e81103157dd73c66dfb5db
Dave Stearns <dlsinfo@uw.edu>
2018-05-14 11:29:57 -0700

    halfway through mqs

--- mq/publish/main.go
+++ mq/publish/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/streadway/amqp"
 )
 
 const usage = `
@@ -24,4 +26,37 @@ func main() {
 		fmt.Println(usage)
 		os.Exit(1)
 	}
+	mqAddr := reqEnv("MQADDR")
+	mqName := reqEnv("MQNAME")
+	message := os.Args[1]
+
+	conn, err := amqp.Dial("amqp://" + mqAddr)
+	if err != nil {
+		log.Fatalf("error dialing MQ: %v", err)
+	}
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("error getting channel: %v", err)
+	}
+	q, err := channel.QueueDeclare(mqName,
+		false, //non-durable
+		false, //won't auto-delete q after idle
+		false, //non-exclusive
+		false, //wait for the server to respond
+		nil)   //server-specific args
+	if err != nil {
+		log.Fatalf("error declaring queue: %v", err)
+	}
+	channel.Confirm(false)
+	channel.Publish("", //default exchange
+		q.Name,
+		true,  //must be added to a queue
+		false, //subscriber doesn't need to read right away
+		amqp.Publishing{
+			Body: []byte(message),
+		})
+
+	log.Printf("sent message: %s", message)
+	channel.Close()
+	conn.Close()
 }
--- mq/subscribe/main.go
+++ mq/subscribe/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/streadway/amqp"
 )
 
 const maxConnRetries = 5
@@ -16,5 +18,40 @@ func reqEnv(name string) string {
 }
 
 func main() {
+	mqAddr := reqEnv("MQADDR")
+	mqName := reqEnv("MQNAME")
 
+	conn, err := amqp.Dial("amqp://" + mqAddr)
+	if err != nil {
+		log.Fatalf("error dialing MQ: %v", err)
+	}
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("error getting channel: %v", err)
+	}
+	q, err := channel.QueueDeclare(mqName,
+		false, //non-durable
+		false, //won't auto-delete q after idle
+		false, //non-exclusive
+		false, //wait for the server to respond
+		nil)   //server-specific args
+	if err != nil {
+		log.Fatalf("error declaring queue: %v", err)
+	}
+
+	msgs, err := channel.Consume(q.Name, //name of the queue
+		"",    //name of this consumer
+		false, //don't want to auto-ack
+		false, //non-exclusive
+		false, //noLocal (not supported by RabbitMQ)
+		false, //wait for the server to respond before delivering
+		nil)   //extra server-specific args
+	if err != nil {
+		log.Fatalf("error consuming messages: %v", err)
+	}
+
+	for msg := range msgs {
+		log.Printf("received message: %s", string(msg.Body))
+		msg.Ack(false)
+	}
 }
