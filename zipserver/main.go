6175f1c377c6fa9d8a4e6e53b43800b20bf63f2b
John HWANG <johnhwang@Johns-MacBook-Air.local>
2018-10-01 12:28:56 -0700

    Modify In Class Exercise for Zipserver

--- zipserver/main.go
+++ zipserver/main.go
@@ -1,8 +1,41 @@
 package main
 
+/*
+  Import relevant packages.
+  HINT: You may need encoding/json package.
+*/
+import (
+  "net/http"
+)
+
 func main() {
 	//TODO: load the zip codes from "zips.csv"
 	//build a ZipIndex on the City field
 	//and start a web server that responds with
 	//all the Zips for a given city name
+
+  //create a new mux (router)
+  //the mux calls different functions for
+  //different resource paths
+  mux := http.NewServeMux()
+
+  // register your handler function
+  mux.HandleFunc("/", handleFunc)
+
+  http.ListenAndServe(":4000", mux)
+}
+
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+  /**
+    Modify this handler function to
+    1) return a JSON of Zipcode objects
+    2) server CORS requests
+  */
+  if r.Method == "GET" {
+    header := w.Header()
+    header.Set("Content-Type", "text/html; charset=utf-8")
+    w.Write([]byte("Hello"))
+  } else {
+    http.Error(w, "Only GET allowed.", http.StatusBadRequest)
+  }
 }
