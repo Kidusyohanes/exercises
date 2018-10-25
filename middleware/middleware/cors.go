52d289dfa3a69af4367907314da8fccbcf5fa53e
John Hwang <4810452+jhwang5@users.noreply.github.com>
2018-10-24 20:30:08 -0700

    Update cors.go

--- middleware/middleware/cors.go
+++ middleware/middleware/cors.go
@@ -3,5 +3,11 @@ package middleware
 
 // TODO: 
 // Create a middleware that 
-// logs the request's method and all its headers
-// for every request
+// adds "Access-Control-Allow-Origin: *" header for every method
+
+
+// Write the middleware in three flavors
+
+// 1) Wrapping around the whole Mux
+// 2) Wrapping around an individual handler function
+// 3) As an adapter, returning a handler
