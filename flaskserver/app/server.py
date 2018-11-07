ac9eca7fb089aa2f3e1fc61aabc4e9c9abd71d05
John HWANG <jhwang5@alumni.stanford.edu>
2018-11-07 10:27:52 -0800

    starter code

--- flaskserver/app/server.py
+++ flaskserver/app/server.py
@@ -1,21 +1,48 @@
-from flask import Flask, request
+from flask import Flask, request, g, jsonify, abort, make_response, current_app
+from bson.json_util import dumps
 
 from pymongo import MongoClient
 DB_URL = 'mongodb://127.0.0.1:27017/'
-client = MongoClient(DB_URL)
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "Hello World!"
 
 
-todos_db = client["todos"]
-todos_collection = todos_db.todos
+@app.route("/todo", methods=["GET", "POST"])
+def todos():
+	if request.method == "GET":
+		return "TBD"
+	elif request.method == "POST":
+		return "TBD"
+	else:
+		abort(405)
 
-todos_collection.insert_one({"title": "My Title", "description": "This is a description", "done": False})
+
+@app.route("/todo/<int: todoid>", methods=["GET", "PUT", "DELETE"])
+def todo():
+	return "TBD"
 
 
-print( [ x for x in todos_collection.find({}) ])
+def get_db():
+	if not hasattr(current_app, "db"):
+		print("Connecting to MongoDB")
+		db = MongoClient(DB_URL)
+		current_app.db = db["todos"]
+
+	return current_app.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+
+
+
 
